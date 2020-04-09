d449c075c6781557e5381ded2519c01d3b0eba93
pr7h <prthpatel412@gmail.com>
2020-04-09 17:07:33 -0400

    Deleting old app

--- Intro2Node/app.js
+++
@@ -1,8 +0,0 @@
-var seeder = require("faker");
-
-console.log("~~~Welcome to MyShop~~~");
-
-for(i=0;i<=10;i++){
-  console.log(seeder.commerce.productName() + " - $" +seeder.commerce.price());
-}
-
