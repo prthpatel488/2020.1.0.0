fa5ca3d3859335d6089da12ca15b8b551145b102
pr7h <prthpatel412@gmail.com>
2020-04-15 13:20:47 -0400

    express Intro

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-  res.send("is this thing working ?");
+  res.send("is this thing working? are you sure???");
+});
+
+
+app.get("/r/:routeparam", (req,res) => {
+  console.log(req.params);
+  res.send("this is from param" + " " + req.params.routeparam);
+});
+
+app.get("/r/:routeparam/comment/:id/:title", (req,res) => {
+  console.log(req.params);
+  res.send("this is from id.. title");
+});
+
+app.get('*', (req,res) => {
+  res.send("Its not you, its me... :(");
 });
 
 app.listen(8000, () =>{
   console.log("Server connnected!");
 });
-
